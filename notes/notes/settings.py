a60d2d674f87fccb3858f2cb9a96bcf2da2edbf5
drejmin <djjamn@gmail.com>
2023-08-14 15:41:39 -0700

    modified:   notes/notes/settings.py

--- notes/notes/settings.py
+++ notes/notes/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'notes_on_notes'
+    'notes_on_notes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
