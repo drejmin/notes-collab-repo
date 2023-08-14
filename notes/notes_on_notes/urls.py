6f8778ca472c29f916f63fcecd266162e9c2b7b7
LeepDev <hyungy@gmail.com>
2023-08-14 18:17:01 -0500

    added note detail

--- notes/notes_on_notes/urls.py
+++ notes/notes_on_notes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.home, name= 'home')
+    path('notes/<int:pk>/', views.NoteDetail.as_view(), name='notes_detail'),
+
 ]
--- notes/notes_on_notes/views.py
+++ notes/notes_on_notes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.views.generic import DetailView
+
 from .models import Note
 
-
 # Create your views here.
+class NoteDetail(DetailView):
+  model = Note
