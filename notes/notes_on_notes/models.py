49bf352a3f14a58bfe27ee45b0c385163e1bf140
drejmin <djjamn@gmail.com>
2023-08-14 15:48:56 -0700

    modified:   notes/notes_on_notes/models.py

--- notes/notes_on_notes/models.py
+++ notes/notes_on_notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Note(models.Model):
+    title= models.CharField(max_length=30)
+    content= models.TextField(max_length= 500)
+    date_created = date.today() 
+    last_modified= date.today()
