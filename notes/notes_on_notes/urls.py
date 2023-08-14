a91a885999178067ce01777e821e2c2927cd5d1a
asp <amandasueputney@gmail.com>
2023-08-14 16:19:54 -0700

    starts index and home page

--- notes/notes_on_notes/urls.py
+++ notes/notes_on_notes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns =[
     path('', views.home, name= 'home')
+    path('notes/', views.notes_on_notes_index, name='index'),
 ]
--- notes/notes_on_notes/views.py
+++ notes/notes_on_notes/views.py
@@ -3,3 +3,8 @@ from .models import Note
 
 
 # Create your views here.
+def notes_on_notes_index(request):
+  notes = Note.objects.all()
+  return render(request, 'notes_on_notes/index.html', {
+    'notes': note
+  })
